6bc0a766c3b185ffd0ef222885148f366b4621a6
johanneswittenburg <johanneswittenburg@gmail.com>
2023-06-12 13:36:56 +0200

    add BuildMac.sh docker-build-script

---
+++ BuildMac.sh
@@ -0,0 +1,10 @@
+Script für Mac - DockerContainer für Deploy -
+cd frontend
+npm ci
+npm run build
+rm -rf ../backend/src/main/resources/static
+mv build ../backend/src/main/resources/static
+cd ../backend
+./mvnw clean package
+cd ..
+docker build -t cratego-docker .
