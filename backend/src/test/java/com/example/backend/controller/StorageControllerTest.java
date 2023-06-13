0d7dc7dd950adb233288e23b5fbc8953fd5f8f98
johanneswittenburg <johanneswittenburg@gmail.com>
2023-06-13 17:17:54 +0200

    add Unit- & Integration-Tests for methods: getStorages & addStorage

--- backend/src/test/java/com/example/backend/controller/StorageControllerTest.java
+++ backend/src/test/java/com/example/backend/controller/StorageControllerTest.java
@@ -4,23 +4,54 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-
 class StorageControllerTest {
 
     @Autowired
     MockMvc mvc;
 
     @Test
-    void getStorages() {
+    @DirtiesContext
+    void getStorages() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/api/storages"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
     }
 
     @Test
-    void addStorage() {
+    @DirtiesContext
+    void addStorage() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/api/storage/new")
+                .contentType("application/json")
+                .content("""
+                                {
+                                    "id": "055",
+                                    "description": "feathers",
+                                    "crts_org": 49,
+                                    "crts_now": 32
+                                }
+                            """))
+            .andExpect(status().isOk());
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/storages"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                            [
+                                {
+                                    "id": "055",
+                                    "description": "feathers",
+                                    "crts_org": 49,
+                                    "crts_now": 32
+                                }
+                            ]
+                            """)).andExpect(jsonPath("$[0].id").isNotEmpty());
     }
 }
--- backend/src/test/java/com/example/backend/service/StorageServiceTest.java
+++ backend/src/test/java/com/example/backend/service/StorageServiceTest.java
@@ -1,16 +1,40 @@
 package com.example.backend.service;
 
+import com.example.backend.model.Storage;
+import com.example.backend.repo.StorageRepo;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 
 class StorageServiceTest {
 
+    StorageRepo repo = mock(StorageRepo.class);
+    StorageService service = new StorageService(repo);
+
     @Test
     void getStorages() {
+        //GIVEN
+        when(repo.findAll()).thenReturn(List.of());
+        //WHEN
+        List<Storage> actual = service.getStorages();
+        //THEN
+        assertEquals(List.of(), actual);
+        verify(repo).findAll();
     }
 
     @Test
     void addStorage() {
+    //GIVEN
+        Storage asserted = new Storage("055", "feathers", 49, 32);
+        when(repo.save(asserted)).thenReturn(asserted);
+    //WHEN
+        Storage actual = repo.save(asserted);
+    //THEN
+        assertEquals(asserted, actual);
+        verify(repo).save(asserted);
     }
 }
