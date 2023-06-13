e596f0812a7a740b91e2eb0cb81bb7431433415e
johanneswittenburg <johanneswittenburg@gmail.com>
2023-06-13 09:16:32 +0200

    add structure for Storage & getStorages-method

--- backend/src/main/java/com/example/backend/controller/helloWorldCotroller.java
+++ backend/src/main/java/com/example/backend/controller/helloWorldCotroller.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/")
+@RequestMapping("/api/")
 public class helloWorldCotroller {
 
-    @GetMapping("/hello")
+    @GetMapping("hello")
     public String sayHello(){
         return "helloWorld";
     }
