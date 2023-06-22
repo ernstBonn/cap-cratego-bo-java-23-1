90b911b1b3d8c50f31434d660df97ab17dd4acc2
johanneswittenburg <johanneswittenburg@gmail.com>
2023-06-22 08:12:41 +0200

    add User.java

---
+++ backend/src/main/java/com/example/backend/model/User.java
@@ -0,0 +1,25 @@
+package com.example.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document("Users")
+public class User {
+
+    private String id;
+    private String name;
+    private String password;
+    @DBRef
+    private List<Storage> storages;
+
+}
